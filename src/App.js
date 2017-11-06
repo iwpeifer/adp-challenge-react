1d166dbb55e85751d99a3a3cd532189f56f2200b
Isaac Peifer <iwpeifer@gmail.com>
2017-11-06 16:42:49 -0500

    added link to repo

--- src/App.js
+++ src/App.js
@@ -73,9 +73,11 @@ class App extends Component {
 
   renderInfo = () => {
     return (
-      <p className='info'>
-        This app utilizes a provided JSON object that contains four Star Wars API (swapi.co) endpoints. I was tasked with retrieving information about each film the provided characters have been in, making sure that info was displayed only after *all* data was loaded. 
-      </p>
+      <div className='info'>
+        <p>This app utilizes a provided JSON object that contains four Star Wars API (swapi.co) endpoints. I was tasked with retrieving information about each film the provided characters have been in, making sure that info was displayed only after *all* data was loaded.</p>
+        <p>Check out the Github repo here:</p>
+        <a href='https://github.com/iwpeifer/adp-challenge-react' target="_blank">GitHub Repo</a>
+      </div>
     )
   }
 
