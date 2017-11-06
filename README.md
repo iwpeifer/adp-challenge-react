1461773bb80eb60d5b27aba346d0485041dab2d7
Isaac Peifer <iwpeifer@gmail.com>
2017-11-06 17:15:49 -0500

    added detail to info

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This app utilizes a provided JSON object that contains four Star Wars API (swapi.co) endpoints. I was tasked with retrieving information about each film the provided characters have been in, making sure that info was displayed only after *all* data was loaded.
+
+Made with React.
--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
       <div className='info'>
         <p>This app utilizes a provided JSON object that contains four Star Wars API (swapi.co) endpoints. I was tasked with retrieving information about each film the provided characters have been in, making sure that info was displayed only after *all* data was loaded.</p>
         <p>Click on a character to see a list of films, click on a film to see information.</p>
-        <p>Check out the Github repo here:</p>
+        <p>Made with React! Check out the Github repo here:</p>
         <a href='https://github.com/iwpeifer/adp-challenge-react' target="_blank" rel="noopener noreferrer">GitHub Repo</a>
       </div>
     )
